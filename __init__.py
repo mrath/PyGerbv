d52f71a076b23f9ed6873984309d9a61e676a0d2
Mikkel Rath Pedersen <mrp@teknologisk.dk>
2018-04-19 11:25:42 +0200

    added access to _libgerbv

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .gerbv import Project, FileInfo, Image, Aperture
+from .gerbv import Project, FileInfo, Image, Aperture, _libgerbv
 from .enumeration import ApertureType
 from .exceptions import *
 from .structure import *
